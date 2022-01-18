ae6ccc2831f9ee02e70bc498b669e613f61dc256
Dietmar Klaus <gitlab-gsa@riseup.net>
2022-01-18 10:18:47 +0000

    Update run/start.sh

--- run/start.sh
+++ run/start.sh
@@ -1,23 +1,26 @@
 #!/bin/ash
 echo "INIT"
-[[ -z "$SLEEPINTER" ]]   &&  SLEEPINTER=90
-[[ -z "$GIT_REPO_KEY" ]]   &&  echo "NO KEY ;CANNOT RUN"
-[[ -z "$GIT_REPO_KEY" ]]   &&  exit 1
+[[ -z "$SLEEPINTER" ]]        &&  SLEEPINTER=90
+[[ -z "$GIT_REPO_KEY" ]]      &&  echo "NO KEY ;CANNOT RUN"
+[[ -z "$GIT_REPO_KEY" ]]      &&  exit 1
 [[ -z "$GIT_REPO_PUBKEY" ]]   &&  echo "NO PuBKEY ;CANNOT RUN"
 [[ -z "$GIT_REPO_PUBKEY" ]]   &&  exit 1
-[[ -z "$GIT_REPO_SYNC" ]]  &&  echo "NO REPO;CANNOT RUN"
-[[ -z "$GIT_REPO_SYNC" ]]  &&  exit 1
+[[ -z "$GIT_REPO_SYNC" ]]     &&  echo "NO REPO;CANNOT RUN"
+[[ -z "$GIT_REPO_SYNC" ]]     &&  exit 1
 
 [[ -z "$BACKUP_PATH" ]] && BACKUP_PATH=/tmp/backup
+[[ -z "$BACKUP_PATH" ]] && BACKUP_PATH=/tmp/backup
+
 mkdir ~/.ssh -p
 #apk add --no-cache git bash openssh-client
-
+[[ -z "$GIT_PATH" ]] || GIT_PATH=/srv/var
 echo "$GIT_REPO_PUBKEY"|base64 -d > ~/.ssh/id_rsa.pub
 echo "$GIT_REPO_KEY"   |base64 -d > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa.pub ~/.ssh/id_rsa
 ## keyscan
 oneline() { tr -d '\n' ; } ;
 
+test -e $GIT_PATH || mkdir $GIT_PATH
 ssh-keyscan  gitlab.com >>  ~/.ssh/known_hosts 2>&1 | oneline
 ssh-keyscan  github.com >>  ~/.ssh/known_hosts  2>&1 | oneline
 
@@ -35,10 +38,10 @@ git config user.email "you@example.com" &>/dev/null
 git push $@  2>&1|grep -v -e "Warning: Permanently added the RSA host key for IP address " -e "To "; } ;
 
 
-myclone ${GIT_REPO_SYNC}  ${STORE_BOLT_PATH} || mkdir -p ${STORE_BOLT_PATH}
+myclone ${GIT_REPO_SYNC}  ${GIT_PATH} || mkdir -p ${STORE_BOLT_PATH}
 [[ -z "$GIT_REPO_BACKUP" ]] || myclone ${GIT_REPO_BACKUP} "$BACKUP_PATH" 
 mkdir -p ${STORE_BOLT_PATH}
-( sleep 10; [[ -z "$SLEEPINTER" ]]   &&  SLEEPINTER=90; while (true);do ( cd ${STORE_BOLT_PATH} ; git add -A  ;git commit -m $(date +%F_%T)"auto"; mypush ) ; [[ -z "$GIT_REPO_BACKUP" ]] || ( cd "$BACKUP_PATH" ; git add -A  ;git commit -m $(date +%F_%T)"auto" ;mypush )  ; sleep $SLEEPINTER ; done ) &  
+( sleep 10; [[ -z "$SLEEPINTER" ]]   &&  SLEEPINTER=90; while (true);do ( cd ${GIT_PATH} ; git add -A  ;git commit -m $(date +%F_%T)"auto"; mypush ) ; [[ -z "$GIT_REPO_BACKUP" ]] || ( cd "$BACKUP_PATH" ; git add -A  ;git commit -m $(date +%F_%T)"auto" ;mypush )  ; sleep $SLEEPINTER ; done ) &  
 
 echo "PREP"
 #cat /init.orig.sh
